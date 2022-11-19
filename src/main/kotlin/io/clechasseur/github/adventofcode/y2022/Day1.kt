a16d3d59c746eec5a8d62d2937180cac2de603b7
Charles Lechasseur <shiftingbeard@gmx.com>
2022-11-18 23:06:25 -0500

    Day 1 skeleton + project structure

---
+++ src/main/kotlin/io/clechasseur/github/adventofcode/y2022/Day1.kt
@@ -0,0 +1,5 @@
+package io.clechasseur.github.adventofcode.y2022
+
+object Day1 {
+    fun part1(): Int = 0
+}
---
+++ src/test/kotlin/io/github/clechasseur/adventofcode/y2022/AdventOfCode2022.kt
@@ -0,0 +1,14 @@
+package io.github.clechasseur.adventofcode.y2022
+
+import io.clechasseur.github.adventofcode.y2022.Day1
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class AdventOfCode2022 {
+    class Day1Puzzles {
+        @Test
+        fun `day 1, part 1`() {
+            assertEquals(0, Day1.part1())
+        }
+    }
+}
