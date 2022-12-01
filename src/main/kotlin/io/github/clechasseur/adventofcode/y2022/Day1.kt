ade8876201792c8e60c7e8be3519bae4276e1780
Charles Lechasseur <shiftingbeard@gmx.com>
2022-12-01 00:25:00 -0500

    Day 1

--- src/main/kotlin/io/github/clechasseur/adventofcode/y2022/Day1.kt
+++ src/main/kotlin/io/github/clechasseur/adventofcode/y2022/Day1.kt
@@ -1,5 +1,33 @@
 package io.github.clechasseur.adventofcode.y2022
 
+import io.github.clechasseur.adventofcode.y2022.data.Day1Data
+
 object Day1 {
-    fun part1(): Int = 0
+    private val input = Day1Data.input
+
+    fun part1(): Int = input.toElves().maxOf { it.calories }
+
+    fun part2(): Int = input.toElves().sortedByDescending { it.calories }.take(3).sumOf { it.calories }
+
+    private data class Elf(val food: List<Int>) {
+        val calories: Int
+            get() = food.sum()
+    }
+
+    private fun String.toElves(): List<Elf> {
+        val food = mutableListOf<Int>()
+        val elves = mutableListOf<Elf>()
+        for (line in lines()) {
+            if (line.isEmpty()) {
+                elves.add(Elf(food.toList()))
+                food.clear()
+            } else {
+                food.add(line.toInt())
+            }
+        }
+        if (food.isNotEmpty()) {
+            elves.add(Elf(food))
+        }
+        return elves
+    }
 }
--- src/test/kotlin/io/github/clechasseur/adventofcode/y2022/AdventOfCode2022.kt
+++ src/test/kotlin/io/github/clechasseur/adventofcode/y2022/AdventOfCode2022.kt
@@ -7,7 +7,12 @@ class AdventOfCode2022 {
     class Day1Puzzles {
         @Test
         fun `day 1, part 1`() {
-            assertEquals(0, Day1.part1())
+            assertEquals(73211, Day1.part1())
+        }
+
+        @Test
+        fun `day 1, part 2`() {
+            assertEquals(213958, Day1.part2())
         }
     }
 }
