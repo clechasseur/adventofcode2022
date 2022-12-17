f55752e063a48c514e68717f3fabaf37b5c44740
Charles Lechasseur <shiftingbeard@gmx.com>
2022-12-16 22:40:09 -0500

    Day 16 part 2 (skeleton)

--- src/main/kotlin/io/github/clechasseur/adventofcode/y2022/Day16.kt
+++ src/main/kotlin/io/github/clechasseur/adventofcode/y2022/Day16.kt
@@ -9,13 +9,10 @@ object Day16 {
 
     private val valveRegex = """^Valve ([A-Z]{2}) has flow rate=(\d+); tunnels? leads? to valves? ([A-Z]{2}(?:, [A-Z]{2})*)$""".toRegex()
 
-    fun part1(): Int = simulate(State(
-        network = Network(input.lines().map { it.toValve() }),
-        pos = "AA",
-        open = emptySet(),
-        minutesRemaining = 30,
-        releasedPressure = 0
-    )).maxOf { it.releasedPressure }
+    fun part1(): Int = simulate(
+        Network(input.lines().map { it.toValve() }),
+        State(pos = listOf("AA"), open = emptySet(), minutesRemaining = 30, releasedPressure = 0)
+    ).maxOf { it.releasedPressure }
 
     private data class Valve(val id: String, val flowRate: Int, val tunnels: List<String>)
 
@@ -31,45 +28,22 @@ object Day16 {
     }
 
     private data class State(
-        val network: Network,
-        val pos: String,
+        val pos: List<String>,
         val open: Set<String>,
         val minutesRemaining: Int,
         val releasedPressure: Int
     ) {
-        fun nextMoves(): List<State> {
+        fun nextMoves(network: Network): List<State> {
             if (minutesRemaining == 0) {
                 return emptyList()
             }
 
-            val stillClosed = network.workingValves - open
-            if (stillClosed.isEmpty()) {
-                return listOf(copy(
-                    pos = "AA",
-                    minutesRemaining = 0,
-                    releasedPressure = releasedPressure + network.workingValves.values.sumOf { valve ->
-                        valve.flowRate * minutesRemaining
-                    }
-                ))
-            }
-
-            val (dist, _) = Dijkstra.build(network, pos)
-            val moves = stillClosed.values.mapNotNull { valve ->
-                val valveDist = (dist[valve.id]!! + 1).toInt()
-                if (valveDist > minutesRemaining) null else {
-                    copy(
-                        pos = valve.id,
-                        open = open + valve.id,
-                        minutesRemaining = minutesRemaining - valveDist,
-                        releasedPressure = releasedPressure + open.sumOf { openValve ->
-                            network.valves[openValve]!!.flowRate * valveDist
-                        }
-                    )
-                }
-            }
+            val stillClosed = network.workingValves.keys - open
+            val dij = pos.map { it to Dijkstra.build(network, it) }
+            val moves = movesForPos(network, pos, dij, stillClosed).toList()
             if (moves.isEmpty()) {
                 return listOf(copy(
-                    pos = "AA",
+                    pos = List(pos.size) { "AA" },
                     minutesRemaining = 0,
                     releasedPressure = releasedPressure + open.sumOf { openValve ->
                         network.valves[openValve]!!.flowRate * minutesRemaining
@@ -78,14 +52,66 @@ object Day16 {
             }
             return moves
         }
+
+        private fun movesForPos(
+            network: Network,
+            remainingPos: List<String>,
+            dij: List<Pair<String, Dijkstra.Output<String>>>,
+            stillClosed: Set<String>,
+            matches: List<Pair<String, Pair<String, Int>>> = emptyList(),
+            unmoving: List<String> = emptyList()
+        ): Sequence<State> {
+            if (remainingPos.isEmpty() || stillClosed.isEmpty()) {
+                if (matches.isEmpty()) {
+                    return emptySequence()
+                }
+
+                val winner = matches.minBy { it.second.second }
+                val newPos = matches.map { match ->
+                    if (match == winner) match.second.first else {
+                        val matchDij = dij.single { it.first == match.first }.second
+                        val path = Dijkstra.assemblePath(matchDij.prev, match.first, match.second.first)!!
+                        path.asSequence().take(winner.second.second + 1).last()
+                    }
+                } + remainingPos + unmoving
+                return sequenceOf(copy(
+                    pos = newPos.sorted(),
+                    open = open + winner.second.first,
+                    minutesRemaining = minutesRemaining - winner.second.second,
+                    releasedPressure = releasedPressure + open.sumOf { openValve ->
+                        network.valves[openValve]!!.flowRate * winner.second.second
+                    }
+                ))
+            }
+
+            val curPos = remainingPos.first()
+            val otherPos = remainingPos.drop(1)
+            val curDij = dij.single { it.first == curPos }.second
+            val dist = stillClosed.mapNotNull { closedValve ->
+                val valveDist = curDij.dist[closedValve]!!.toInt() + 1
+                if (valveDist <= minutesRemaining) closedValve to valveDist else null
+            }.toMap()
+            return dist.entries.asSequence().flatMap { (closedValve, valveDist) ->
+                movesForPos(
+                    network,
+                    otherPos,
+                    dij,
+                    stillClosed - closedValve,
+                    matches + (curPos to (closedValve to valveDist)),
+                    unmoving
+                )
+            } + if (dist.size < remainingPos.size) {
+                movesForPos(network, otherPos, dij, stillClosed, matches, unmoving + curPos)
+            } else emptySequence()
+        }
     }
 
-    private fun simulate(initialState: State): Set<State> {
+    private fun simulate(network: Network, initialState: State): Set<State> {
         var states = setOf(initialState)
         val finalStates = mutableSetOf<State>()
         while (states.isNotEmpty()) {
             val (nextStates, newFinalStates) = states.flatMap {
-                it.nextMoves()
+                it.nextMoves(network)
             }.partition {
                 it.minutesRemaining > 0
             }
